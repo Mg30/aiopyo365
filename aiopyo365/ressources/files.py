db4a50d4552e61d8c52500e1b96d9b9115147249
gonza <matgonzalez@hotmail.fr>
2022-12-14 15:57:26 +0100

    refactor: extract code from class to move it to facade class sharepoint service

--- aiopyo365/ressources/files.py
+++ aiopyo365/ressources/files.py
@@ -1,16 +1,13 @@
-"""Module that provide office 365 services by exposing classes
-that encapsulate GRAPH microsoft API calls.
+""" API to Microsoft Graph resources types for working with files:
 
-https://docs.microsoft.com/en-us/graph/overview?view=graph-rest-1.0
+Drive - Represents a logical container of files, like a document library or a user's OneDrive.
+DriveItem - Represents an item within a drive, like a document, photo, video, or folder.
 
-available services:
-    - Sharepoint
+https://learn.microsoft.com/en-us/graph/api/resources/onedrive?view=graph-rest-1.0
 """
 
 
 import aiohttp
-import os
-import aiopyo365.clients.factories as factories
 from dataclasses import dataclass, field
 from typing import Coroutine, Generator, Literal
 
@@ -26,10 +23,10 @@ class DriveItems(object):
         site_name: name of sharepoint site to interact with
 
     """
+
     base_url: str
     session: aiohttp.ClientSession
     _max_upload_size: int = field(init=False, default=60000000)
-
 
     async def list_children(self, item_id: str) -> Coroutine:
         """List all children items from item_id.
@@ -62,36 +59,18 @@ class DriveItems(object):
             resp.raise_for_status()
             return await resp.json()
 
-    async def upload(self, file_path: str, file_name: str) -> None:
-        """Upload file to sharepoint.
-
-        Arg(s):
-            path: path of the file to be uploaded
-            file_name: name to give to the file in sharepoint when uploaded
-
-        """
-        file_size = os.path.getsize(file_path)
-        if file_size < 4000000:
-            await self._upload_small_file(file_path, file_name)
-        else:
-            await self._upload_large_file(
-                file_path, file_size, file_name, conflict_behavior="replace"
-            )
-
-
-    async def _upload_small_file(self, path: str, file_name: str) -> Coroutine:
+    async def upload_small_file(self, content: bytes, file_name: str) -> Coroutine:
         """Upload file less than 4 MB to sharepoint.
 
         ref: https://docs.microsoft.com/en-us/graph/api/driveitem-put-content?view=graph-rest-1.0&tabs=http
 
         Arg(s):
-            path: path of the file to be uploaded
+            content: content of the file as bytes
             file_name: name to give to the file in sharepoint when uploaded
         Return:
             A request Response object
         """
         endpoint = f"{self.base_url}/drive/items/root:/{file_name}:/content"
-        content = self._read_file(path)
         headers = {"Content-Type": "application/octet-stream"}
         async with self.session.put(
             f"{endpoint}", headers=headers, data=content
@@ -99,9 +78,9 @@ class DriveItems(object):
             resp.raise_for_status()
             return await resp.json()
 
-    async def _upload_large_file(
+    async def upload_large_file(
         self,
-        file_path: str,
+        content: bytes,
         file_byte_size: int,
         filename: str,
         conflict_behavior: Literal["fail", "replace", "rename"] = "fail",
@@ -111,7 +90,7 @@ class DriveItems(object):
         ref: https://docs.microsoft.com/en-us/graph/api/driveitem-createuploadsession?view=graph-rest-1.0#upload-bytes-to-the-upload-session
 
         Arg(s):
-            file_path: file path to be uploaded
+            content: content of the file as bytes
             file_byte_size: size of the file to be uploaded in bytes
             filename: name to give to the file in sharepoint when uploaded
             conflict_behavior: how to handle a file that has already the same name should be one of fail, replace, rename
@@ -121,18 +100,14 @@ class DriveItems(object):
         """
         resp = await self._create_upload_session(filename, conflict_behavior)
         upload_url = resp["uploadUrl"]
-        if file_byte_size < self._max_upload_size:
-            content = self._read_file(file_path)
-            headers = {
-                "Content-Type": "application/octet-stream",
-                "Content-Length": f"{file_byte_size}",
-                "Content-Range": f"bytes 0-{file_byte_size-1}/{file_byte_size}",
-            }
-            async with self.session.put(
-                upload_url, data=content, headers=headers
-            ) as resp:
-                resp.raise_for_status()
-                return await resp.json()
+        headers = {
+            "Content-Type": "application/octet-stream",
+            "Content-Length": f"{file_byte_size}",
+            "Content-Range": f"bytes 0-{file_byte_size-1}/{file_byte_size}",
+        }
+        async with self.session.put(upload_url, data=content, headers=headers) as resp:
+            resp.raise_for_status()
+            return await resp.json()
 
     async def _create_upload_session(
         self,
@@ -156,30 +131,8 @@ class DriveItems(object):
             }
         }
         async with self.session.post(
-            f"{self._base_url}/drive/items/root:/{upload_filename}:/createUploadSession",
+            f"{self.base_url}/drive/items/root:/{upload_filename}:/createUploadSession",
             params=str(data),
         ) as resp:
             resp.raise_for_status()
             return await resp.json()
-
-    def _read_file(self, path: str) -> bytes:
-        """Read file as bytes
-
-        Arg:
-            path: path of the file to read
-
-
-            content of the file as bytes
-        """
-        with open(path, "rb") as f:
-            content = f.read()
-            return content
-
-    def _bytes_from_file(self, filename, chunksize) -> Generator[bytes, None, None]:
-
-        with open(filename, "rb") as f:
-            data = f.read(chunksize)
-            yield data
-            while data:
-                data = f.read(chunksize)
-                yield data
--- aiopyo365/services/sharepoint.py
+++ aiopyo365/services/sharepoint.py
@@ -1,8 +1,12 @@
-from dataclasses import dataclass, field
-
 import aiohttp
+import os
 from aiopyo365.providers.auth import GraphAuthProvider
-from aiopyo365.clients.factories import DriveItemsSitesFactory
+from aiopyo365.factories.drive_items import DriveItemsSitesFactory
+from aiopyo365.factories.sites import SitesFactory
+from aiopyo365.ressources.files import DriveItems
+from aiopyo365.ressources.sites import Site
+from dataclasses import dataclass, field
+from typing import Coroutine
 
 
 @dataclass
@@ -10,19 +14,65 @@ class SharePointService(object):
     auth_provider: GraphAuthProvider
     hostname: str
     site_name: str
+    _site_client: Site = field(init=False)
+    _drive_items_client: DriveItems = field(init=False)
+    session: aiohttp.ClientSession = field(init=False)
 
     async def __aenter__(self):
         auth_header = await self.auth_provider.auth()
         self.session = aiohttp.ClientSession(headers=auth_header)
-        drive_item_site = DriveItemsSitesFactory()
-        self.sharepoint = await SharePointClient.create(
-            self.hostname,
-            self.site_name,
-            session=self.session,
+
+        self._site_client = SitesFactory().create(session=self.session)
+        site_id = await self.get_site_id()
+
+        self._drive_items_client = DriveItemsSitesFactory(site_id=site_id).create(
+            session=self.session
         )
-        return self.sharepoint
+        return self
 
     async def __aexit__(self, *err):
         await self.session.close()
         self.session = None
- 
+
+    async def get_site_id(self) -> str:
+        """Couritne to fetch the site id given hostname and site_name
+
+        Returns:
+            str: representing the side id
+        """
+        resp = await self._site_client.get_sites_by_server_relative_url(
+            hostname=self.hostname, site_name=self.site_name
+        )
+        return resp["id"]
+
+    async def upload(
+        self, file_path: str, file_name: str, conflict_behavior="fail"
+    ) -> Coroutine:
+        """Upload file to sharepoint
+
+        Arg(s):
+            path: path of the file to be uploaded
+            file_name: name to give to the file in sharepoint when uploaded
+
+        """
+        content = self._read_file_as_bytes(file_path)
+        file_byte_size = os.path.getsize(file_path)
+        if file_byte_size < 4000000:
+            return await self._drive_items_client.upload_small_file(content, file_name)
+        else:
+            return await self._drive_items_client.upload_large_file(
+                content, file_byte_size, file_name, conflict_behavior=conflict_behavior
+            )
+
+    def _read_file_as_bytes(self, path: str) -> bytes:
+        """Read a file at path and return its content as bytes
+
+        Args:
+            path (str): to the file to read content from
+
+        Returns:
+            bytes: content of the file
+        """
+        with open(path, "rb") as f:
+            content = f.read()
+            return content
